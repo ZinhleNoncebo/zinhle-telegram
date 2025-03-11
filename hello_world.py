5b19837826ca66b4209e3ada054d27a69fed2fcb
ZinhleNoncebo <zinhlenoncebo96@gmail.com>
2025-03-11 17:54:00 +0200

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+# Prompt user to input their name
+name= input("Enter your name")
+#Store the user's input in the "name" variable once they have inputed
+print(name)
+#prompt user to input their age
+age=input("Enter your age")
+#Store the user's input in the "age" variable once they have inputed
+print(age)
+print("Hello, World!")
