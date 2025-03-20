a10d7f73a8a2db77aad861b72febfe03d40cd180
ZinhleNoncebo <zinhlenoncebo96@gmail.com>
2025-03-20 15:36:07 +0200

    Add files via upload

---
+++ details.py
@@ -0,0 +1,6 @@
+name=input("Enter your name: ")
+age=input("Enter your age: ")
+house_number=input("Enter your house number: ")
+street_name=input("Enter your street name: ")
+print("This is {}. She is {} years old and lives at house number 67{} on {}.".format(name,age,house_number,street_name))
+
