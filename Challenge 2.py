5011593ed04ac901c0b2b2ea15a648c287990979
ZinhleNoncebo <zinhlenoncebo96@gmail.com>
2025-04-08 17:33:18 +0200

    Add files via upload

---
+++ Challenge 2.py
@@ -0,0 +1,6 @@
+string_fav=input("What is your favourite restuarant?:")
+int_fav=int(input("What is your favourite number?"))
+print(string_fav)
+print(int_fav)
+string_fav2=int(string_fav)
+#I get an error when trying to convert string_fav into an integer because integers are numbers (both negetive and positive) and words cannot be converted into numerical values.
