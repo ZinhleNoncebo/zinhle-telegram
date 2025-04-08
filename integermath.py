53f4f9b690853356d24c9b67274b1f14fcfc9ebb
ZinhleNoncebo <zinhlenoncebo96@gmail.com>
2025-04-08 17:32:17 +0200

    Add files via upload

---
+++ integermath.py
@@ -0,0 +1,16 @@
+number1=int(input("Enter the first number:"))
+number2=int(input("Enter the second number:"))
+number3=int(input("Enter the third number:"))
+
+print("The sum of {0} and {1} and {2}: {3}".format (number1,number2,number3,number2+number1+number3))
+print("The results of {0} minus {1}:{2}".format(number1,number2,number1-number2))
+print("The results of {0} multiplied by{1}:{2}".format(number1,number3, number1*number3))
+
+
+result = (number1 + number2 + number3) / number3
+print("The sum of {0}, {1}, and {2} divided by {3} is: {4}".format(number1, number2, number3, number3, result))
+
+
+
+
+
